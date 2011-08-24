349093a108b98cf99cadc44183ef2b086895566d
pjammer <philipjingram@yahoo.com>
2011-08-24 10:28:15 -0400

    adding generators into rails 3 style and moving rake tasks to appear in rake -T

--- lib/generators/flotomatic/flotomatic_generator.rb
+++ lib/generators/flotomatic/flotomatic_generator.rb
@@ -2,14 +2,10 @@
 # Copyright:: MigraineLiving.com
 # License::   Distributed under the same terms as Ruby
 
-class FlotomaticGenerator < Rails::Generator::Base # :nodoc:
-  def manifest
-    record do |m|
-      m.file 'flotomatic.css', "public/stylesheets/flotomatic.css"
-      m.directory 'public/javascripts/flotomatic'
-      %w(excanvas.js excanvas.min.js flotomatic.js jquery.colorhelpers.js jquery.colorhelpers.min.js jquery.flot.crosshair.js jquery.flot.crosshair.min.js jquery.flot.image.js jquery.flot.image.min.js jquery.flot.js jquery.flot.min.js jquery.flot.navigate.js jquery.flot.navigate.min.js jquery.flot.selection.js jquery.flot.selection.min.js jquery.flot.stack.js jquery.flot.stack.min.js jquery.flot.threshold.js jquery.flot.threshold.min.js jquery.js jquery.min.js).each do |file|
-        m.file "flotomatic/#{file}", "public/javascripts/flotomatic/#{file}"
-      end
-    end
-  end
+class FlotomaticGenerator < Rails::Generators::Base
+  desc "Description: Copies flotomatic javascript directory and css file to the proper application public directory structure, as needed by flotmatic."
+  
+  #it appears source_root is set now in Rails::Generators::Base, let's see :-)
+  copy_file 'flotomatic.css', "public/stylesheets/flotomatic.css"
+  directory "flotomatic, "'public/javascripts/flotomatic' 
 end
